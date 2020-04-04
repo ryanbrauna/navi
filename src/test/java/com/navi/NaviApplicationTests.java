d73b14b1a0a6268bd47dc3b812e6b3eeaecc9145
Ryan <ryan.brauna@bandtec.com.br>
2020-04-03 22:10:38 -0300

    create structure

---
+++ src/test/java/com/navi/NaviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
