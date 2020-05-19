1aefefba616e6f98e0933ae006ce1ef3b78b8e1f
Marcelo Antony <macosta1@stefanini.com>
2020-05-19 16:59:08 -0300

    cad

--- frontend/navi/src/Cadastro.js
+++ frontend/navi/src/Cadastro.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './css/Cadastro.css';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 // Components
 import NavbarInst from './NavbarInst';
@@ -14,76 +14,171 @@ import {
     Form,
     Button
 } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class Cadastro extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = this.initialState;
+        this.cadChange = this.cadChange.bind(this);
+        this.submitCad = this.submitCad.bind(this);
+    }
+
+    initialState = {
+        tipo: "",
+        nome: "",
+        CPF: "",
+        email: "",
+        telefone: "",
+        senha: "",
+        confSenha: "",
+        show: false
+    }
+
+    submitCad = event => {
+        alert(
+            "Tipo:" + this.state.tipo +
+            "\nNome:" + this.state.nome +
+            "\nCPF:" + this.state.CPF +
+            "\nEmail:" + this.state.email +
+            "\nTelefone:" + this.state.telefone +
+            "\nSenha:" + this.state.senha +
+            "\nConfSenha:" + this.state.confSenha
+        );
+    }
+
+    cadChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    showForm(tipo) {
+        this.setState({
+            tipo: tipo,
+            show: true
+        });
+    }
+
     render() {
+        const { nome, CPF, email, telefone, senha, confSenha } = this.state;
+
         return (
             <div className="body">
                 <NavbarInst />
 
                 <Container>
-                    <div className="cadastro p-5 rounded shadow">
+                    <div className="cadastro px-5 py-3 rounded shadow">
                         <h4 className="mb-3">Cadastro</h4>
-                        <Form>
+                        <Form onSubmit={this.submitCad} id="formCad">
                             <Row>
                                 <Col sm="12">
                                     <p>Selecione o tipo de cadastro:</p>
                                 </Col>
                                 <Col>
-                                    {['radio'].map((type) => (
-                                        <div key={`inline-${type}`} className="mb-3">
-                                            <Form.Check className="text-primary mr-5" inline label="Vendedor" type={type} id={`inline-${type}-1`} />
-                                            <Form.Check className="text-primary mr-5" inline label="Comprador" type={type} id={`inline-${type}-2`} />
-                                        </div>
-                                    ))}
+                                    <Form.Check inline
+                                        name="tipo"
+                                        id={`tipoV`}
+                                        className="text-primary mr-5"
+                                        label="Vendedor"
+                                        type="radio"
+                                        onClick={() => {
+                                            this.showForm("V")
+                                        }}
+                                    />
+                                    <Form.Check inline
+                                        name="tipo"
+                                        id={`tipoC`}
+                                        className="text-primary mr-5"
+                                        label="Comprador"
+                                        type="radio"
+                                        onClick={() => {
+                                            this.showForm("C")
+                                        }}
+                                    />
                                 </Col>
                             </Row>
+                            <div style={{ display: + this.state.show ? "block" : "none" }}>
+                                <Form.Row>
+                                    <Form.Group className="mr-4" as={Col} controlId="formNome">
+                                        <Form.Control required
+                                            placeholder="Nome"
+                                            name="nome"
+                                            value={nome}
+                                            onChange={this.cadChange}
+                                        />
+                                        <Form.Label>Digite seu Nome.</Form.Label>
+                                    </Form.Group>
 
-                            <Form.Row>
-                                <Form.Group className="mr-4" as={Col} controlId="formNome">
-                                    <Form.Control placeholder="Nome" />
-                                    <Form.Label>Digite seu Nome.</Form.Label>
-                                </Form.Group>
+                                    <Form.Group className="mr-4" as={Col} controlId="formCPF">
+                                        <Form.Control required
+                                            placeholder="CPF"
+                                            name="CPF"
+                                            value={CPF}
+                                            onChange={this.cadChange}
+                                        />
+                                        <Form.Label>Digite seu CPF.</Form.Label>
+                                    </Form.Group>
+                                </Form.Row>
 
-                                <Form.Group className="mr-4" as={Col} controlId="formCPF">
-                                    <Form.Control placeholder="CPF" />
-                                    <Form.Label>Digite seu CPF.</Form.Label>
-                                </Form.Group>
-                            </Form.Row>
+                                <Form.Row>
+                                    <Form.Group className="mr-4" as={Col} controlId="formEmail">
+                                        <Form.Control required
+                                            type="email"
+                                            placeholder="Email"
+                                            name="email"
+                                            value={email}
+                                            onChange={this.cadChange}
+                                        />
+                                        <Form.Label>Digite seu endereço de E-mail.</Form.Label>
+                                    </Form.Group>
 
-                            <Form.Row>
-                                <Form.Group className="mr-4" as={Col} controlId="formEmail">
-                                    <Form.Control type="email" placeholder="Email" />
-                                    <Form.Label>Digite seu endereço de E-mail.</Form.Label>
-                                </Form.Group>
+                                    <Form.Group className="mr-4" as={Col} controlId="formTelefone">
+                                        <Form.Control
+                                            placeholder="Telefone"
+                                            name="telefone"
+                                            value={telefone}
+                                            onChange={this.cadChange}
+                                        />
+                                        <Form.Label>Digite seu Telefone.</Form.Label>
+                                    </Form.Group>
+                                </Form.Row>
 
-                                <Form.Group className="mr-4" as={Col} controlId="formTelefone">
-                                    <Form.Control placeholder="Telefone" />
-                                    <Form.Label>Digite seu Telefone.</Form.Label>
-                                </Form.Group>
-                            </Form.Row>
+                                <Form.Row>
+                                    <Form.Group className="mr-4" as={Col} controlId="formSenha">
+                                        <Form.Control required
+                                            type="password"
+                                            placeholder="Senha"
+                                            name="senha"
+                                            value={senha}
+                                            onChange={this.cadChange}
+                                        />
+                                        <Form.Label>Digite uma senha Senha.</Form.Label>
+                                    </Form.Group>
 
-                            <Form.Row>
-                                <Form.Group className="mr-4" as={Col} controlId="formSenha">
-                                    <Form.Control type="password" placeholder="Senha" />
-                                    <Form.Label>Digite uma senha Senha.</Form.Label>
-                                </Form.Group>
+                                    <Form.Group className="mr-4" as={Col} controlId="formConfSenha">
+                                        <Form.Control required
+                                            type="password"
+                                            placeholder="Confirmação de Senha"
+                                            name="confSenha"
+                                            value={confSenha}
+                                            onChange={this.cadChange}
+                                        />
+                                        <Form.Label>Confirme a senha Senha.</Form.Label>
+                                    </Form.Group>
+                                </Form.Row>
 
-                                <Form.Group className="mr-4" as={Col} controlId="formConfSenha">
-                                    <Form.Control type="password" placeholder="Confirmação de Senha" />
-                                    <Form.Label>Confirme a senha Senha.</Form.Label>
-                                </Form.Group>
-                            </Form.Row>
-
-                            <Link className="text-light" to="/login">
+                                {/* <Link className="text-light" to="/login"> */}
                                 <Button className="mt-3 px-5" variant="primary" type="submit">Cadastrar</Button>
-                            </Link>
+                                {/* </Link> */}
+                            </div>
                         </Form>
                     </div>
                 </Container>
 
-                <div id="fim">
+                <div id="fim" className="fixed-bottom">
                     <span> NAVI © 2020</span>
                 </div >
             </div>
