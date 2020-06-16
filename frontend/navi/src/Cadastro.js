2c2ca6c6e86d8e4c0ec38c6166e29ec55f69a4ce
jefrain419 <jefrain419@gmail.com>
2020-06-16 00:06:23 -0300

    alterando a mask do CNPJ

--- frontend/navi/src/Cadastro.js
+++ frontend/navi/src/Cadastro.js
@@ -36,7 +36,7 @@ class Cadastro extends Component {
     componentDidMount() {
         $('#cpf').mask('000.000.000-00', { reverse: false });
         $('#telefone').mask('(00) 00000-0000');
-        $('#cnpj').mask('00.000.000/0000-00', { reverse: false });
+        $('#cnpj').mask('00.000.000.0000-00', { reverse: false });
         $('#cep').mask('00000-000');
 
 
